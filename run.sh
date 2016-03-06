8c076ff1101d2a91e70042368f580abffc33c61f
Michał Tomczak <michal@tomczak.net.pl>
2016-03-06 18:06:38 +0100

    add config script

---
+++ run.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -e
+
+su - postgres -c '/usr/pgsql-9.1/bin/postgres -D /var/lib/pgsql/data &'
+sleep 5
+
+if [ ! -f "/started" ]
+then
+	sql='/tmp/init.sql'
+
+	echo '' > $sql
+	if [ "$DBNAME" ] 
+	then
+		echo "CREATE DATABASE $DBNAME;" >> "$sql"
+	fi
+
+	if [ "$DBUSER" ] 
+	then
+		parts=($(echo $DBUSER | tr ":" "\t"))
+
+		echo "CREATE USER ${parts[0]}" >> "$sql"
+		if [ "$parts[1]" ]
+		then
+			echo " WITH PASSWORD '${parts[1]}'" >> "$sql"
+		fi
+
+			echo " SUPERUSER;" >> "$sql"
+
+	fi
+	touch '/started'	
+	su - postgres -c 'psql < '$sql''
+
+fi
+	
+
+/bin/bash
